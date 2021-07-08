05ea5f320a06cbd5481a65953ca076091ea85b3e
IchikonWD <ezequielmigueles@icloud.com>
2021-07-08 17:41:44 +0200

    Quiz Template

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
-document.querySelector('body').innerHTML = 'Holi!'
+const respuestas = {
+  director: "Shigeru Miyamoto y Takashi Tezuka",
+  date: "1987",
+  inspiration: "Sus propias aventuras",
+  triforce: "Poder, sabiduría y coraje",
+  temple: "El templo de la Luz",
+};
+
+// document.querySelector('body').innerHTML = 'Holi!'
